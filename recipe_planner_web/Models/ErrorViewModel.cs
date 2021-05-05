4df15c4a507dfe9af3ffe183b11a4f60d20fea20
Vladyslav Makartet <vladyslav.makartet@gmail.com>
2021-05-05 12:12:53 +0200

    Add project files.

---
+++ recipe_planner_web/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace recipe_planner_web.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
